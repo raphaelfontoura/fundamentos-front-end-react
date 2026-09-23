aee63c7f320e756f1c750abcc19f422e248f92c6
Raphael Fontoura <raphael.fontoura@gmail.com>
2026-09-22 23:20:26 -0300

    feat: add error handling and validation to login form

--- src/app/(aula-4)/login/page.tsx
+++ src/app/(aula-4)/login/page.tsx
@@ -9,15 +9,24 @@ export default function LoginPage() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const { login, isReady } = useAuth();
+    const [error, setError] = useState("");
     const router = useRouter();
 
     const handleSubmit = async (e: React.SubmitEvent) => {
         e.preventDefault();
+        setError("");
+
+        if (!email || !password) {
+            setError("Preencha o formulário para continuar.");
+            return;
+        }
+
         try {
             await login(email, password);
             router.replace("/dashboard");
         } catch (err) {
-            console.error(err);
+            console.log(err)
+            setError((err as Error).message);
         }
     };
 
@@ -27,6 +36,7 @@ export default function LoginPage() {
         <div className="flex min-h-[calc(100dvh-4rem)] w-full items-center justify-center">
             <div className="mx-auto flex w-full max-w-md flex-col items-center gap-y-4 rounded border p-4">
                 <h1 className="text-3xl font-bold">Login</h1>
+                { error && <p className="p-2 bg-red-900 text-white font-bold border rounded">{error}</p> }
                 <form onSubmit={handleSubmit} className="grid w-full gap-y-2">
                     <input
                         className="w-full rounded border border-gray-600 p-2"
