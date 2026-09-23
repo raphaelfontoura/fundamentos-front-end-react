64f9b592b27a09b659bd02a4f4c404621ed9dbb0
Raphael Fontoura <raphael.fontoura@gmail.com>
2026-09-22 23:50:06 -0300

    feat: conditionally render user role information in unauthorized page

--- src/app/(aula-4)/unauthorized/page.tsx
+++ src/app/(aula-4)/unauthorized/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
     return (
         <div className="grid gap-y-4 w-96 p-4 boder border-gray-600 rounded">
             <h1 className="text-3xl font-bold">Não autorizado</h1>
-            <p> Sua role: {user?.role}</p>
+            { user && <p> Sua role: {user?.role}</p> }
             <Link
             className="bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 border rounded font-bold text-center"
              href={"/login"}>
